7b00771530112444019711f33f79ebf02509186a
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-04-29 00:39:06 +0530

    about me page added

--- scripts/gsap.js
+++ scripts/gsap.js
@@ -47,8 +47,8 @@ headertl.from('#avwork',{
 headertl.from('.word',{
     y:20,
     opacity:0,
-    duration:0.2,
-    stagger:0.1,
+    duration:1.2,
+    stagger:0.2,
     transform:" rotate(6deg)",
     ease: "expoScale(0.5,7,none)",
 })
@@ -61,3 +61,40 @@ headertl.from('#headerimg',{
 
 }
 headerani()
+
+// this animation for aboutme page 
+function aboutme(){
+const text1 = new SplitType('#pani', { types: 'words' })
+const abouttl = gsap.timeline()
+abouttl.from('.word',{
+    y:10,
+    opacity:0,
+    duration:0.2,
+    stagger:0.1,
+    transform:" rotate(5deg)",
+    ease: "expoScale(0.5,7,none)",
+})
+abouttl.from('#secp',{
+    y:10,
+    opacity:0,
+    duration:0.2,
+    ease: "expoScale(0.5,7,none)",
+})
+abouttl.from('#chatbtn',{
+    y:10,
+    opacity:0,
+    duration:1,
+    stagger:0.1,
+    transform:" rotate(5deg)",
+    ease: "expoScale(0.5,7,none)",
+})
+abouttl.from('#aboutnavli li',{
+    y:20,
+    opacity:0,
+    duration:0.6,
+    stagger:0.2,
+    transform:" rotate(15deg)",
+    ease: "expoScale(0.5,7,none)",
+})
+}
+aboutme()
